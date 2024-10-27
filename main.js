11541e290e800f196bdc2be336672dc398f857b9
k <kyle.offenbacher@gmail.com>
2024-10-27 14:02:55 -0400

    Add event listener for product selection and quantity input

---
+++ main.js
@@ -0,0 +1,11 @@
+//cc 11
+
+//task 1 - html code
+
+//task 2
+
+//event listeners for changes in input
+const productSelector = document.getElementById('product-selector');
+const quantityInput = document.getElementById('quantity');
+const totalPriceElement = document.getElementById('total-price');
+
